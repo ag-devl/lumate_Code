f5152f74040cb00675bc739d40d95cfcdb7d0b0f
Ubuntu <ubuntu@ip-10-73-160-230.ec2.internal>
2013-10-19 11:03:32 +0000

    Second Commit

---
+++ guestbook/views.py
@@ -0,0 +1,22 @@
+from guestbook.models import Visitor
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+# Create your views here.	
+def index(request):	
+	f_name = request.POST.get('firstname') 
+	l_name = request.POST.get('lastname')
+	try:
+	        if f_name:
+			f_name = request.POST.get('firstname')
+        		l_name = request.POST.get('lastname')
+			p = Visitor(first_name = f_name,last_name = l_name)
+			p.save()
+	except KeyError:
+		f_name = 'default'
+	return render_to_response ('guestbook/index.html',context_instance=RequestContext(request))	
+
+
+def entry(request):
+	visitor_array = Visitor.objects.all()
+	return render_to_response ('guestbook/entry.html', {'visitor_list': visitor_array})
